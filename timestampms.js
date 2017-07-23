9e50f07c190adf603b98eef5829e084c42da675e
System <none>
2017-07-23 08:35:48 +0000

    better implementation

--- timestampms.js
+++ timestampms.js
@@ -1,35 +1,26 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
-var bodyparser = require('body-parser');
-app.use(bodyparser.urlencoded({extended: false}));
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-app.use(function(req, res) {
-  if (req.query.length > 0) {
-    var time = "";
-    var result = {"unixtimestamp": null, "date": null};
-    req.query.forEach(function(value){
-      if (value) {
-        time = value;
-        if (isNaN(time)) {
-          //proceed as if time string
-          var date = new Date(time);
-          if (date) {
-            result.date = time;
-            result.unixtimestamp = date.getTime();
-          }
-        }
-        else {
-          //proceed as if timestamp
-          result.unixtimestamp = time;
-          result.date = new Date(time).toDateString();
-        }
-        return false;
-      }
-    });
+app.use(function(req, res, next) {
+  if (req.url && req.url.length > 1) {
+    var time = req.url.substring(1);
+    var result = {"unix": null, "natural": null};
+    var date = new Date(time);
+    if (date) {
+      result.natural = months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
+      result.unix = date.getTime();
+    }
     res.send(JSON.stringify(result));
+  }
+  else if (next) {
+    next();
   }
 });
 
-app.listen(12345);
+app.use(express.static(path.join(path.join(__dirname,"views"), "index.html")));
+
+app.listen(80);
